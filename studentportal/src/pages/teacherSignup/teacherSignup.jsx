ef203c2fdfd1cb86dc3522ceaec6773c631ebd5d
ALI <2155399@roc-dev.com>
2025-03-27 13:04:26 +0100

    teachersignup placeholder

--- studentportal/src/pages/teacherSignup/teacherSignup.jsx
+++ studentportal/src/pages/teacherSignup/teacherSignup.jsx
@@ -1,10 +1,26 @@
 import { Fragment } from 'react';
+import Navbar from '../../components/navbar';
 
 const TeacherSignup = () => {
   return (
     <Fragment>
       <div className='mainContainer'>
-        <h1>test</h1>
+        <Navbar />
+        <div className='loginContainer'>
+          <div className='loginCard'>
+            <h2>Sign Up for a Teacher Account</h2>
+            <form className='loginForm'>
+              <input type='text' placeholder='Full Name ' />
+              <input type='text' placeholder='Email' />
+              <input type='password' placeholder='Password' />
+              <input type='password' placeholder='Confirm Password' />
+              <input type='text' placeholder='Secret Code' />
+              <button className='loginButton' type='submit'>
+                Log In
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     </Fragment>
   );
